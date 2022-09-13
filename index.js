f66301b594e6b0aa544736ac5631da34e65cf907
ZhykharievAnatolii <106694298+ZhykharievAnatolii@users.noreply.github.com>
2022-09-13 12:20:52 +0300

    donw new files

---
+++ index.js
@@ -0,0 +1,34 @@
+"use strict"
+// alert('Hi Anatolii');
+// alert(`Hi all`);
+// const antoliiZhykhariev='Anatolii Zhykhariev';
+// alert (antoliiZhykhariev);
+// let age=34;
+// alert(age);
+// age=35;
+// alert(age);
+// const number=null;
+// // const number=0;
+// alert(number)
+// confirm(`Are you sure?`);
+// const number= 3%4;
+// alert(number);
+// const name= prompt(`Enter your name`);
+// const surname=prompt(`Enter your surname`);
+// // const result=`Hi,`+name+` `+surname;
+// const result=`Hi,${name} ${surname}`
+// alert(result)
+// alert(name);
+// alert(surname);
+// alert(prompt(`Enter your name`));
+// const nameOfUser=prompt(`Enter your name,please`);
+// const invitation=`Hello,${nameOfUser} ! How are you?`;
+// alert(invitation)
+const nameOfUser=prompt('Please enter your name');
+confirm(`Are you sure?`);
+const ageOfyourWife=prompt(`How old your wife?`);
+confirm(`Are you sure?`);
+const ageOfUser=+prompt(`Plese enter your age`);
+confirm(`Are you sure?`);
+const resultMessage=`Hi,${nameOfUser},${ageOfUser} , your wife is ${ageOfyourWife} years old. How are you?`;
+alert(resultMessage)
