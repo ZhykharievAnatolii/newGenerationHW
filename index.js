d4394eae24c69ff2d65b8a262151abf9e6a20b96
ZhykharievAnatolii <106694298+ZhykharievAnatolii@users.noreply.github.com>
2022-09-13 15:55:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict"
+// const firstNumber=+prompt(`Please enter your first number`);
+// const secondNumber=+prompt(`Please enter your first number`);
+// const result=firstNumber+secondNumber;
+// alert(result)
+const firstNumber= Number(prompt(`Please enter your number`));
+confirm(`Are you sure?`);
+const secondNumber=+prompt(`Please enter your second number`);
+confirm(`Are you sure?`);
+const result=firstNumber+secondNumber;
+const message=`It is your result = ${result}`
+alert(message);
