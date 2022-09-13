e13ae14c296bda674b5956b7ac69b597f9f17217
ZhykharievAnatolii <106694298+ZhykharievAnatolii@users.noreply.github.com>
2022-09-13 17:27:40 +0300

    added new files for my homework

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict"
+// const firstNumber=+prompt(`Please enter your first number`);
+// const secondNumber=+prompt(`Please enter your first number`);
+// const result=firstNumber+secondNumber;
+// alert(result)
+// const firstNumber= Number(prompt(`Please enter your number`));
+// confirm(`Are you sure?`);
+// const secondNumber=+prompt(`Please enter your second number`);
+// confirm(`Are you sure?`);
+// const result=firstNumber+secondNumber;
+// const message=`It is your result = ${result}`
+// alert(message);
+const radius=+prompt(`Please enter a circle radius`);
+confirm(`Are you sure?`)
+const result=radius*radius*Math.PI;
+const addedResult=`Your result is ${result}`
+alert(addedResult)
