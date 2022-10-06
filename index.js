37032fb813255d200b8591786822d7110b51e604
ZhykharievAnatolii <106694298+ZhykharievAnatolii@users.noreply.github.com>
2022-10-06 13:57:58 +0300

    upload new files ver 1.0

---
+++ index.js
@@ -0,0 +1,72 @@
+"use strict"
+// const obj={
+//     adress:`haharina`,
+//     street:24,
+//     house:2,
+//     name:`Anatolii`,
+// };
+// // for(let propertyName in obj){
+// //     console.log(propertyName);
+// // };--------------------- так выводим ключи(свойства)
+// // for(let propertyName in obj){
+// //     console.log(obj[propertyName]);}---------так выводим значения
+// // for(let propertyName in obj){
+// //     console.log(propertyName,obj[propertyName]);}---- а так выводим все вместе
+// const arr=[];
+// const names=[`Bohdan`,`Yulia`,`Kirill`];
+// while(true){
+//     const name=prompt(`Please enter your name`);
+//     if(name===`123`){
+//         continue;
+//     }
+//     if(name===`break`|| name===null){
+//         break;
+//     };
+//     names.push(name);
+// }
+// names[1];
+// const books=[
+//     {name:`It`,
+//         author:`Steven King`},
+//     {name:`Lorem`,
+//         author:`John Doe`},];
+// books.push({name:`Kys`,author:`Kit`});
+// function createBook(name, author) {
+//     return {
+//         name,
+//         author,
+//     }
+//
+// };
+// books.push(createBook(`My another book`, `Another author`));
+
+// books[0];
+// books[0].author;
+// for(let index in names){
+//     console.log(names[index])
+// };
+// for(let index in names){
+//     console.log(names[index])
+// };
+// names[3]=`Yura`;
+// names[names.length]=`Anatolii`;
+// names[names.length]={name:`Fuck`,
+//     author:`Off`
+// };
+// names.push(`Volli`);
+// numbers.push(2,2,23,4,5,6,7,2,5,7,8,1);
+// const names=[`Bohdan`,`Yulia`,`Kirill`];
+// names.unshift(`Elka`,`Palka`);
+// const students=[`Oleksii`,`Olena`];
+// // const mergedArr=[];
+// // mergedArr.concat(students,names);
+// const mergedArr=names.concat(students);
+// const nam=[`alina`,`yasya`,`vasya`];
+// // nam.pop();удалить один елемент в конце
+// // nam.push(`kyka`);-добавить с конца
+// // nam.unshift(`kaka`);добавить с начала
+// // nam.concat(`kyka kaka`); добавить в конец, любые данные
+// // nam.shift(); удалить один елемент в начале
+// nam.indexOf(`yasya`);
+
+
