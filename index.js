a918457e3eb3b528541710abbe24fe9bf10746b3
ZhykharievAnatolii <106694298+ZhykharievAnatolii@users.noreply.github.com>
2022-11-10 11:42:39 +0200

    upl new files
    files about hw 18 ii is about DOM , BOM

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+function anatolii(){
+
+};
+window.anatolii();
+window.navigator
+navigator.appVersion
+'5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+screen
+window.screen
+window.location
+location.href
+window.location.href
+'https://lms.ithillel.ua/groups/62a9b1cf07905d280bc039b4/lessons/62a9b1cf07905d280bc039da'
+location.host
+'lms.ithillel.ua'
+// location.href='https://google.com'- таким образом можем сделать редирект
+// const paragraph=document.querySelector
+// const paragraph=document.querySelector('p')-так цепляемся к параграфу, чтобы к классу, ставим точку
+// const paragraph=document.querySelector('.welcomeText')
+// если есть несколько елементов одинаковых, р, див, то он обратится к первому
+paragraph.parentElement
+// const lin=document.querySelector(".welcomeText");
+// const header=lin.parentElement
+// const element=document.querySelector('p');
+// element.parentElement.parentElement.parentElement.parentElement.parentElement
+// element.parentElement.parentElement.parentElement.children
+// document.querySelector('table')
+// document.querySelectorAll('li');
+// document.getElementsByClassName('newInfo');
+// document.getElementById('second');
+// document.body.firstElementChild.lastElementChild.lastElementChild
+// const diver=document.getElementsByTagName('div')
